87a03e2a381b56d510ae526572ef6d70de73ab9f
dharunrajs <dharunraj003@gmail.com>
2025-02-10 16:09:19 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Demo{
+    public static void main(String[] args)
+    {
+        Scanner copy=new Scanner(System.in);
+        int mark1,mark2,mark3;
+        mark1=scanner.nextInt;
+        mark2=scanner.nextInt;
+        mark3=scanner.nextInt;
+        if(mark1>=35)
+        {
+            System.out.println("PASS");
+        }
+        else if (mark2>=35) {
+            System.out.println("PASS");
+        }
+        else if (mark3) {
+            System.out.println("PASS");    
+        }
+        else{
+            System.out.println("FAIL");
+        }
+    }
+}
